52676c0f3b05a33d04b2b8363822027cf43e2e40
SDen4 <asw325@yandex.ru>
2022-02-15 20:49:37 +0500

    template

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './App.module.css';
+
+function App() {
+  return (
+    <div className={classes.app}>
+      <h1>Code review todo</h1>
+      <div className={classes.line} />
+    </div>
+  );
+}
+
+export default App;

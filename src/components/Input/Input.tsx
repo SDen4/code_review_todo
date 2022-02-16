3cf3baa10a3264c430c2c90b2b78943bc8cb19e0
SDen4 <asw325@yandex.ru>
2022-02-16 17:58:58 +0500

    logic of add todo

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { addTodo } from '../../store/TodoReducer/actions';
+import { TodoItemType } from '../../store/TodoReducer/types';
 
 import classes from './Input.module.css';
 
 const Input: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const [newTodoText, setNewTodoText] = useState<string>('');
+
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTodoText(event.target.value);
+  };
+
+  const onBtnClick = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    if (!newTodoText) return;
+
+    const newTodo: TodoItemType = {
+      id: String(new Date().getTime()),
+      title: newTodoText,
+      checked: false,
+    };
+
+    dispatch(addTodo(newTodo));
+    setNewTodoText('');
+  };
+
   return (
     <div className={classes.inputWrapper}>
-      <input type="text" />
-      <button type="button">Add</button>
+      <form onSubmit={onBtnClick}>
+        <input type="text" value={newTodoText} onChange={onChangeInput} />
+      </form>
+      <button type="button" onClick={onBtnClick}>
+        Add
+      </button>
     </div>
   );
 };
